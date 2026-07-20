1bb8d61eb89a4379ff86f6af353926c6a004b707
Marioo-Jr <juniorminas2013@gmail.com>
2026-07-20 15:09:04 -0300

    Bean validation anotaçoes basicas

--- dscatalog-/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
+++ dscatalog-/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
@@ -3,15 +3,20 @@ package com.devsuperior.dscatalog.dto;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
-
 import com.devsuperior.dscatalog.entities.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public class UserDTO implements Serializable{
     private static final long serialVersionUID = 1L;
 
     private Long id;
+
+    @NotBlank(message = "Campo obrigatorio")
     private String firstName;
     private String lastName;
+
+    @Email(message = "favor entrar um email valido")
     private String email;
 
     Set<RoleDTO>roles = new HashSet<>();
